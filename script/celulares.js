7d432eba2dcca719f788026840b85b786f4e16bf
Allisson-Sousa-Lira <51099254+Allisson-Sousa-Lira@users.noreply.github.com>
2020-02-18 19:44:52 -0300

    Add files via upload
    Pagina dos celulares + algumas imagens de celulares

---
+++ script/celulares.js
@@ -0,0 +1,69 @@
+//IMAGEM 1
+
+function aparecer1() {
+    document.getElementById("imagem1").style.display='block';
+}
+function sumir1(){
+    document.getElementById("imagem1").style.display='none';
+}
+
+//IMAGEM 2
+
+function aparecer2() {
+    document.getElementById("imagem2").style.display='block';
+}
+function sumir2(){
+    document.getElementById("imagem2").style.display='none';
+}
+
+//IMAGEM 3
+
+function aparecer3() {
+    document.getElementById("imagem3").style.display='block';
+}
+function sumir3(){
+    document.getElementById("imagem3").style.display='none';
+}
+
+//IMAGEM 4
+function aparecer4() {
+    document.getElementById("imagem4").style.display='block';
+}
+function sumir4(){
+    document.getElementById("imagem4").style.display='none';
+}
+
+//IMAGEM 5
+
+function aparecer5() {
+    document.getElementById("imagem5").style.display='block';
+}
+function sumir5(){
+    document.getElementById("imagem5").style.display='none';
+}
+
+//IMAGEM 6
+
+function aparecer6() {
+    document.getElementById("imagem6").style.display='block';
+}
+function sumir6(){
+    document.getElementById("imagem6").style.display='none';
+}
+
+//IMAGEM 7
+
+function aparecer7() {
+    document.getElementById("imagem7").style.display='block';
+}
+function sumir7(){
+    document.getElementById("imagem7").style.display='none';
+}
+//IMAGEM 8
+
+function aparecer8() {
+    document.getElementById("imagem8").style.display='block';
+}
+function sumir8(){
+    document.getElementById("imagem8").style.display='none';
+}
